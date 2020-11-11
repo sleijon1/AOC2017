c5d565089625ddb0b7b8126a12cea6f8b9928c71
sleijon1 <simonleijon59@gmail.com>
2020-11-11 01:16:59 +0100

    part 2

--- d10/d10.py
+++ d10/d10.py
@@ -1,21 +1,17 @@
 import re
-#lengths = [197, 97, 204, 108, 1, 29, 5, 71, 0, 50, 2, 255, 248, 78, 254, 63]
-lengths = "1,2,3"
-new_lengths = []
-for num in map(ord, re.findall(r'\d+', lengths)):
-    new_lengths.append(num)
-    new_lengths.append(ord(','))
-new_lengths = new_lengths[0:-1] + [17, 31, 73, 47, 23]
-print(new_lengths)
-exit()
+from functools import reduce
+#lengths = [197, 97, 204, 108, 1, 29, 5, 71, 0, 50, 2, 255, 248, 78, 254, 63] # Part 1
+lengths = "197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63"
+lengths = [ord(char) for char in lengths]
+lengths = lengths + [17, 31, 73, 47, 23]
 string = [num for num in range(256)]
 # lengths = [3, 4, 1, 5]
 # string = [num for num in range(5)]
 
 def knot_hash(rounds=64):
+    skip_size = 0
+    position = 0
     for _ in range(rounds):
-        skip_size = 0
-        position = 0
         for length in lengths:
             if length + position < len(string):
                 list.reverse(rev_slice := string[position:position + length])
@@ -31,7 +27,26 @@ def knot_hash(rounds=64):
 
             position = (skip_size + length + position) % len(string)
             skip_size += 1
-
+    return string
     #print("Solution part 1: " + str(string[0] * string[1]))
 
-knot_hash()
+def dense_hash(sparse, block_size=16):
+    dense = []
+    for i in range(0, len(sparse), block_size):
+        block = sparse[i:i+block_size]
+        dense.append(reduce(lambda x,y: x^y, block))
+    return dense
+
+sparse_hash = knot_hash()
+dense = dense_hash(sparse_hash)
+hex_dense = ['0' + hex(num).lstrip("0x") if
+             len(hex(num).lstrip("0x")) < 2
+             else hex(num).lstrip("0x")
+             for num in dense]
+
+HASH = ""
+for digit in hex_dense:
+    HASH += digit
+print("Solution part 2, Knot Hash: " + str(HASH))
+print(len(HASH))
+#print(sparse_hash)
