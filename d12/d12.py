bc866b07cc9d2004e20277173864d1f844ce1c57
sleijon1 <simonleijon59@gmail.com>
2020-11-13 00:28:05 +0100

    cleaner solutioin

--- d12/d12.py
+++ d12/d12.py
@@ -10,31 +10,13 @@ for line in lines:
     for p in map(int, endp.split(", ")):
         pipes[int(startp)].append(p)
 
-
-# def count_group():
-#     zero_com = set()
-#     for zcom in pipes[0]:
-#         zero_com.add(zcom)
-#     # fun brute force way of finding all possible
-#     # ids in coms with 0
-#     for i in range(1, 21):
-#         for start in pipes:
-#             if start in zero_com:
-#                 for end in pipes[start]:
-#                     zero_com.add(end)
-#     print(zero_com)
-#     print("Solution part 1: " + str(len(zero_com)))
-
-
 def count_group():
     groups = []
     for pipe in pipes:
-        skip = False
-        for group in groups:
-            if pipe in group:
-                skip = True
-                break
-        if skip:
+        pipe_in = [True if pipe in group else False for group in groups]
+        if any(pipe_in):
+            # already created this pipes group
+            # don't repeat
             continue
         groups.append(create_group(pipe, set()))
     print("Solution part 1: " + str(len(groups[0])))
@@ -50,3 +32,22 @@ def create_group(startp, group):
 
 
 count_group()
+
+
+##########################################
+# Fast brute force way of solving part 1 #
+##########################################
+# def count_group():
+#     zero_com = set()
+#     for zcom in pipes[0]:
+#         zero_com.add(zcom)
+#     # fun brute force way of finding all possible
+#     # ids in coms with 0
+#     for i in range(1, 21):
+#         for start in pipes:
+#             if start in zero_com:
+#                 for end in pipes[start]:
+#                     zero_com.add(end)
+#     print(zero_com)
+#     print("Solution part 1: " + str(len(zero_com)))
+
