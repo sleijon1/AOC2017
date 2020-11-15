53e8111ff6f83d26d75df1e31a317d998190fd9c
sleijon1 <simonleijon59@gmail.com>
2020-11-15 20:11:07 +0100

    d14 done and d10 cleanup

--- d10/d10.py
+++ d10/d10.py
@@ -1,16 +1,12 @@
 import re
 from functools import reduce
-#lengths = [197, 97, 204, 108, 1, 29, 5, 71, 0, 50, 2, 255, 248, 78, 254, 63] # Part 1
-lengths = "197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63"
-lengths = [ord(char) for char in lengths]
-lengths = lengths + [17, 31, 73, 47, 23]
-string = [num for num in range(256)]
-# lengths = [3, 4, 1, 5]
-# string = [num for num in range(5)]
 
-def knot_hash(rounds=64):
+def knot_hash(lengths, rounds=64):
     skip_size = 0
     position = 0
+    lengths = [ord(char) for char in lengths]
+    lengths = lengths + [17, 31, 73, 47, 23]
+    string = [num for num in range(256)]
     for _ in range(rounds):
         for length in lengths:
             if length + position < len(string):
@@ -28,7 +24,6 @@ def knot_hash(rounds=64):
             position = (skip_size + length + position) % len(string)
             skip_size += 1
     return string
-    #print("Solution part 1: " + str(string[0] * string[1]))
 
 def dense_hash(sparse, block_size=16):
     dense = []
@@ -37,16 +32,24 @@ def dense_hash(sparse, block_size=16):
         dense.append(reduce(lambda x,y: x^y, block))
     return dense
 
-sparse_hash = knot_hash()
-dense = dense_hash(sparse_hash)
-hex_dense = ['0' + hex(num).lstrip("0x") if
-             len(hex(num).lstrip("0x")) < 2
-             else hex(num).lstrip("0x")
-             for num in dense]
+def hex_dense(dense_hash):
+    hex_dense = []
+    for num in dense_hash:
+        HEX = hex(num)
+        if len(HEX.lstrip('0x')) == 0:
+            hex_dense.append('00')
+        elif len(HEX.lstrip('0x')) < 2:
+            hex_dense.append('0' + HEX.lstrip('0x'))
+        else:
+            hex_dense.append(HEX.lstrip('0x'))
+    return hex_dense
 
-HASH = ""
-for digit in hex_dense:
-    HASH += digit
-print("Solution part 2, Knot Hash: " + str(HASH))
-print(len(HASH))
-#print(sparse_hash)
+if __name__ == '__main__':
+    inp = "197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63"
+    sparse_hash = knot_hash(inp)
+    dense = dense_hash(sparse_hash)
+    hex_dense = hex_dense(dense)
+    HASH = ""
+    for digit in hex_dense:
+        HASH += digit
+    print("Solution part 2, Knot Hash: " + str(HASH))
--- d14/d14.py
+++ d14/d14.py
@@ -4,23 +4,59 @@ from d10.d10 import *
 
 inp = "a0c2017"
 
+
 def hex_to_bin(hex_data):
-    scale = 16 ## equals to hexadecimal
+    scale = 16  # equals to hexadecimal
     num_of_bits = 4
     bin_data = bin(int(hex_data, scale))[2:].zfill(num_of_bits)
     return bin_data
 
+
 def produce_hash(inp):
     sparse_hash = knot_hash(inp)
     dense = dense_hash(sparse_hash)
-    print(hex_dense(dense))
     HASH = ""
     for digit in hex_dense(dense):
         HASH += digit
     return HASH
 
-def make_grid(INPUT):
-    for i in range(0, 128):
-        pass
 
-make_grid()
+def make_grid(INPUT):
+    used = 0
+    grid = []
+    for i in range(0, 128):
+        row = ""
+        curr_hash = produce_hash(INPUT+'-'+str(i))
+        for char in curr_hash:
+            bin_hash = hex_to_bin(char)
+            bin_hash = ''.join(['#' if dig == '1' else '.' for dig in bin_hash])
+            used += bin_hash.count('#')
+            row += bin_hash
+        grid.append(list(row))
+    print("Solution part 1: " + str(used))
+    return grid
+
+
+def count_regions(grid):
+    current_region = 1
+    for j, row in enumerate(grid):
+        for i, square in enumerate(row):
+            if square != '#':
+                continue
+            (queue := list()).append((i, j))
+            while queue:
+                curr_x, curr_y = queue.pop(0)
+                grid[curr_y][curr_x] = current_region
+                adjacent = [(curr_x-1, curr_y), (curr_x, curr_y-1),
+                            (curr_x+1, curr_y), (curr_x, curr_y+1)]
+                for x, y in adjacent:
+                    if y >= 0 and y < 128 and x >= 0 and x < 128:
+                        if grid[y][x] == '#':
+                            queue.append((x, y))
+            current_region += 1
+    total_regions = current_region - 1 # last cr += 1 doesnt amount to a region
+    print("Solution part 2: " + str(total_regions))
+
+
+grid = make_grid('hfdlxzhv')
+count_regions(grid)
